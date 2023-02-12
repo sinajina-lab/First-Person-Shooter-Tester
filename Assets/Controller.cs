f0ea1cade3e05dbb469d5e7300c597e229ad0b69
Gwidaniel <danielmigwi.gits@gmail.com>
2023-02-12 20:26:12 +0300

    Created Field of view visualisation test scene. Created a new controller script for the player. Changed the cameras perspective to top down view. Added Rigidbody, FieldOfView script and FieldOfViewEditor script to the player.

---
+++ Assets/Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float moveSpeed = 6;
+
+    Rigidbody rb;
+    Camera viewCamera;
+    Vector3 velocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+}
